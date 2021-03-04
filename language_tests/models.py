d3b7a20a0d056d1db72a35360c189f754070fb5e
Artsiom Zhvanko <azhvanko@outlook.com>
2021-03-04 22:32:30 +0300

    Update models.py

--- language_tests/models.py
+++ language_tests/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class LanguageTestType(models.Model):
+    name = models.CharField(
+        max_length=128,
+        unique=True,
+        verbose_name='Тип теста'
+    )
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+
+    objects = models.Manager()
+
+    class Meta:
+        verbose_name = 'Тип теста'
+        verbose_name_plural = 'Типы тестов'
+        ordering = ['id', ]
+
+    def __str__(self):
+        return self.name
