7ab624d27ac601d7171a098e58ffd2bf7170c9f2
Artsiom Zhvanko <azhvanko@outlook.com>
2021-03-06 21:17:13 +0300

    Register Answer, LanguageTestType models

--- language_tests/admin.py
+++ language_tests/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from language_tests.models import Answer, LanguageTestType
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'answer',)
+    list_display_links = ('id', 'answer',)
+    search_fields = ('answer',)
+
+
+class LanguageTestTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'is_published',)
+    list_display_links = ('id', 'name',)
+    list_editable = ('is_published',)
+    search_fields = ('name',)
+
+
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(LanguageTestType, LanguageTestTypeAdmin)
