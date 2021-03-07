9d5275fb0059b9c517a9f6fda5886c3b794b287d
Artsiom Zhvanko <azhvanko@outlook.com>
2021-03-07 22:14:10 +0300

    Add tests

--- language_tests/tests/test_views.py
+++ language_tests/tests/test_views.py
@@ -43,3 +43,27 @@ class LanguageTestListViewTest(LanguageTestMixin, TestCase):
     def test_title_value(self):
         response = self.client.get(reverse(self.path_name))
         self.assertContains(response, '<title>Список тестов</title>')
+
+
+class LanguageTestDetailViewTest(LanguageTestMixin, TestCase):
+    path_name = 'language_test_preview'
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/tests/1/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse(self.path_name, kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse(self.path_name, kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response,
+            f'{self.app_name}/language_test_preview.html'
+        )
+
+    def test_title_value(self):
+        response = self.client.get(reverse(self.path_name, kwargs={'pk': 1}))
+        self.assertContains(response, '<title>test_type_1</title>')
