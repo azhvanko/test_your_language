405872f4467fc4e339a4a6ea7f10dfc2d05e6c50
Artsiom Zhvanko <azhvanko@outlook.com>
2021-03-07 22:09:27 +0300

    Add LanguageTestDetailView; add template

--- language_tests/urls.py
+++ language_tests/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from language_tests.views import LanguageTestListView
+from language_tests.views import LanguageTestDetailView, LanguageTestListView
 
 
 urlpatterns = [
     path('', LanguageTestListView.as_view(), name='language_tests'),
+    path('<int:pk>/', LanguageTestDetailView.as_view(), name='language_test_preview'),
 ]
--- language_tests/views.py
+++ language_tests/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from language_tests.models import LanguageTestType
 
@@ -8,3 +8,10 @@ class LanguageTestListView(ListView):
     model = LanguageTestType
     queryset = LanguageTestType.objects.filter(is_published=True).only('id', 'name')
     template_name = 'language_tests/language_tests.html'
+
+
+class LanguageTestDetailView(DetailView):
+    context_object_name = 'language_test'
+    model = LanguageTestType
+    queryset = LanguageTestType.objects.filter(is_published=True).only('id', 'name')
+    template_name = 'language_tests/language_test_preview.html'
