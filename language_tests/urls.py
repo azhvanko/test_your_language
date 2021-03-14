5a607afb3b85c77f983feac2d991b294dc90a3cb
Artsiom Zhvanko <azhvanko@outlook.com>
2021-03-14 19:48:46 +0300

    Add LanguageTestResultView; update url patterns

--- language_tests/urls.py
+++ language_tests/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from language_tests.views import (
     LanguageTestDetailView,
     LanguageTestListView,
+    LanguageTestResultView,
     LanguageTestView
 )
 
@@ -10,5 +11,6 @@ from language_tests.views import (
 urlpatterns = [
     path('', LanguageTestListView.as_view(), name='language_tests'),
     path('<int:pk>/', LanguageTestDetailView.as_view(), name='language_test_preview'),
-    path('<int:pk>/test', LanguageTestView.as_view(), name='language_test'),
+    path('<int:pk>/test/', LanguageTestView.as_view(), name='language_test'),
+    path('result/', LanguageTestResultView.as_view(), name='test_result'),
 ]
--- language_tests/views.py
+++ language_tests/views.py
@@ -1,10 +1,12 @@
+import json
 from typing import Dict
 
+from django.http import Http404, HttpRequest, JsonResponse
 from django.shortcuts import get_object_or_404
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, View
 
 from language_tests.models import LanguageTestType
-from language_tests.utils import generate_questions_list
+from language_tests.utils import generate_questions_list, get_right_answers
 
 
 class LanguageTestMixin:
@@ -29,7 +31,7 @@ class LanguageTestView(LanguageTestMixin, ListView):
     object_list = None
     template_name = 'language_tests/language_test.html'
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request: HttpRequest, *args, **kwargs):
         get_object_or_404(self.queryset, pk=kwargs['pk'])
         context = self.get_context_data(
             test_type_id=kwargs['pk'],
@@ -49,3 +51,19 @@ class LanguageTestView(LanguageTestMixin, ListView):
         )
 
         return context
+
+
+class LanguageTestResultView(View):
+
+    def get(self,  *args, **kwargs):
+        raise Http404
+
+    def post(self, request: HttpRequest) -> JsonResponse:
+        try:
+            answers = json.loads(request.body.decode('utf-8'))
+        except Exception:
+            right_answers = {}
+        else:
+            right_answers = get_right_answers(answers, request.user.id)
+        finally:
+            return JsonResponse({'data': right_answers})
