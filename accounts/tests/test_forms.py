b4a1dc56680e5b7e1538290886701195d831404f
Artsiom Zhvanko <azhvanko@outlook.com>
2021-03-26 21:13:01 +0300

    Refactoring; add tests

--- accounts/tests/test_forms.py
+++ accounts/tests/test_forms.py
@@ -1,18 +1,89 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from accounts.forms import LoginForm, SignUpForm
+from accounts.forms import DeactivationForm, LoginForm, SignUpForm
+from accounts.tests.utils import AccountsMixin
 
 
-class SignUpFormTest(TestCase):
+class AuthFormsTestCase(AccountsMixin, TestCase):
+    """
+    Helper base class for all the follow test cases.
+    """
+    @classmethod
+    def setUpTestData(cls):
+        cls.active_user = User.objects.create_user(**cls.users['active_user'])
+        cls.new_user = User.objects.create_user(**cls.users['new_user'])
+
+
+class DeactivationFormTest(AuthFormsTestCase):
+    form_fields = {'username', 'password'}
+
+    def test_fields(self):
+        form = DeactivationForm(request=None)
+        self.assertEqual(len(form.fields), len(self.form_fields))
+        self.assertTrue(all(field in self.form_fields for field in form.fields))
+
+    def test_valid_form(self):
+        user = self.users['active_user']
+        form = DeactivationForm(None, user)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.errors, {})
+
+    def test_form_invalid_if_incorrect_password(self):
+        user = dict(self.users['active_user'])
+        user['password'] = 'fake_password'
+        form = DeactivationForm(request=None, data=user)
+        password_error = form.errors.get_json_data()['password'][0]
+        self.assertFalse(form.is_valid())
+        self.assertEqual(password_error['code'], 'invalid_password')
+        self.assertEqual(
+            password_error['message'],
+            str(form.error_messages['invalid_password'])
+        )
+
+
+class LoginFormTest(AuthFormsTestCase):
+    form_fields = {'username', 'password'}
+
+    def test_fields(self):
+        form = LoginForm(request=None)
+        self.assertEqual(len(form.fields), len(self.form_fields))
+        self.assertTrue(all(field in self.form_fields for field in form.fields))
+
+    def test_valid_form(self):
+        user = self.users['active_user']
+        form = LoginForm(None, user)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(form.errors, {})
+
+    def test_form_invalid_if_unconfirmed_email(self):
+        user = self.users['new_user']
+        form = LoginForm(None, user)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.non_field_errors().get_json_data()[0]['code'],
+            'unconfirmed_email'
+        )
+        self.assertEqual(
+            form.non_field_errors().get_json_data()[0]['message'],
+            str(form.error_messages['unconfirmed_email'])
+        )
+
+
+class SignUpFormTest(AuthFormsTestCase):
     form_fields = {'username', 'email', 'password1', 'password2'}
-    fixtures = ['users', ]
-    user = {
+    unregistered_user = {
         'username': 'test_user_0',
         'email': 'test_user_0@example.com',
-        'password1': 'ce09wefadkln9af12345678',
-        'password2': 'ce09wefadkln9af12345678'
+        'password1': 'drowssap_resu_tset',
+        'password2': 'drowssap_resu_tset'
     }
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        assert not User.objects.filter(username=cls.unregistered_user['username'])
+        assert not User.objects.filter(email=cls.unregistered_user['email'])
 
     def test_fields(self):
         form = SignUpForm()
@@ -20,12 +91,13 @@ class SignUpFormTest(TestCase):
         self.assertTrue(all(field in self.form_fields for field in form.fields))
 
     def test_valid_form(self):
-        form = SignUpForm(data=self.user)
+        form = SignUpForm(data=self.unregistered_user)
         self.assertTrue(form.is_valid())
+        self.assertEqual(form.errors, {})
 
-    def test_invalid_form(self):
-        user = dict(self.user)
-        user['email'] = 'test_user_1@example.com'
+    def test_form_invalid_if_registered_email(self):
+        user = dict(self.unregistered_user)
+        user['email'] = self.active_user.email
         form = SignUpForm(data=user)
         self.assertFalse(form.is_valid())
         self.assertEqual(
@@ -35,38 +107,4 @@ class SignUpFormTest(TestCase):
         self.assertEqual(
             form.errors['email'].get_json_data()[0]['message'],
             str(form.error_messages['registered_email'])
-        )
-
-
-class LoginFormTest(TestCase):
-    form_fields = {'username', 'password'}
-    fixtures = ['users', ]
-    user = {
-        'username': 'test_user_1',
-        'password': 'test_password',
-    }
-
-    def test_fields(self):
-        form = LoginForm(request=None)
-        self.assertEqual(len(form.fields), len(self.form_fields))
-        self.assertTrue(all(field in self.form_fields for field in form.fields))
-
-    def test_valid_form(self):
-        form = LoginForm(None, self.user)
-        self.assertTrue(form.is_valid())
-
-    def test_unconfirmed_email(self):
-        user = User.objects.get(username=self.user['username'])
-        user.is_active = False
-        user.last_login = None
-        user.save()
-        form = LoginForm(None, self.user)
-        self.assertFalse(form.is_valid())
-        self.assertEqual(
-            form.non_field_errors().get_json_data()[0]['code'],
-            'unconfirmed_email'
-        )
-        self.assertEqual(
-            form.non_field_errors().get_json_data()[0]['message'],
-            str(form.error_messages['unconfirmed_email'])
         )
